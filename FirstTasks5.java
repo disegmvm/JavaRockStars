4e01b48d1294ef13def200ff86770893fea2867e
disegmvm <dees3g@gmail.com>
2020-10-25 18:59:04 +0500

    Adding 5,6 and 9 tasks

--- FirstTasks5.java
+++ FirstTasks5.java
@@ -3,7 +3,18 @@ public class FirstTasks5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int largest = 0;
+		int secondLargest = 0;
+		int[] array = { 10, 1000, 20, 8, -1, 80, 20, 56, };
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > largest) {
+				secondLargest = largest;
+				largest = array[i];
+			} else if (array[i] > secondLargest) {
+				secondLargest = array[i];
+			}
+		}
+		System.out.println("The 2nd Largest number is: " + secondLargest);
 	}
 
 }
--- FirstTasks6.java
+++ FirstTasks6.java
@@ -3,7 +3,20 @@ public class FirstTasks6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 int [] [] sumNumber = {
+                 {100,88,74},
+                 {99,120,45},
+                 {88,33,1000},
+                 };
+                 sumRowsCols(sumNumber);
+        }
+        public static void sumRowsCols(int[][] row) {
+                 int sum = 0;
+                 for (int i = 0; i < row.length; i++) {
+                          for (int j = 0; j < row[i].length; j++)
+                                   sum = sum + row[i][j];
+                 }
+                 System.out.println("The sum of all numbers is " + sum);
 	}
 
 }
