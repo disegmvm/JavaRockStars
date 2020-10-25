a895bc7a8ea9c0ea03597fcf9f2abfe010d0732c
disegmvm <dees3g@gmail.com>
2020-10-25 20:39:08 +0500

    Fixing task number 4

--- FirstTasks4.java
+++ FirstTasks4.java
@@ -5,10 +5,10 @@ public class FirstTasks4 {
 		// TODO Auto-generated method stub
 		//Maximum and minimum number in the array?
 				
-				 int maximumArray[] = {8,11,12,13,1,25,62,34,54,100,76,87,10,54};
+				 int maximumArray[] = {99999,11,12,13,-456876,25,62,34,54,100,76,87,10,54};
 				 int minimum = maximumArray[0];
 				 int maximum = maximumArray[0];
-				 for(int i = 0; i<13; i++) {
+				 for(int i = 1; i<maximumArray.length; i++) {
 					 
 					 if (maximumArray[i] < minimum) minimum = maximumArray[i];
 					 if (maximumArray[i] > maximum) maximum = maximumArray[i];
